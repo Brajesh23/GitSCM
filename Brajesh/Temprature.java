4c407a5ce61f75f876c6d54008f4b768b6af0ec7
Brajesh <Brajeshmahajan01@gmail.com>
2021-10-06 17:31:38 +0530

    My First Commit

---
+++ Brajesh/Temprature.java
@@ -0,0 +1,18 @@
+package src.main.java.com.impetus.jumbune.web.beans;
+
+	public enum TemperatureCategory {
+
+	    /** The hot. */
+	    HOT,
+
+	    /** The warm. */
+	    WARM,
+
+	    /** The cold. */
+	    COLD,
+
+	    /** The frozen. */
+	    FROZEN;
+	}
+
+
