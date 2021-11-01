db0171ffa5d1715be7063673722be34affc1fd55
Brajesh23 <91937378+Brajesh23@users.noreply.github.com>
2021-11-01 12:53:30 +0530

    myfilescommithivetosnowflake

--- GitSCM/new/Benchmarking.java
+++
@@ -1,5 +0,0 @@
-Records		Chart Data				Dashboard Data
-
-4152682  ->   35 se 55 seconds                  ->   1 min 40 sec
-4185888  ->   30 seconds to 45 seconds		->   20 ms
-4185888  ->   25 seconds to 40 seconds          ->   20 ms
