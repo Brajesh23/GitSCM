1bb22f01fd252ee1138c3428317ede2f5400cc81
Brajesh23 <91937378+Brajesh23@users.noreply.github.com>
2021-11-08 16:59:22 +0530

    Spark2

--- queries/Hive/testdemo2.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO insert_test SELECT * FROM insert_test2;
-INSERT INTO insert_test SELECT * FROM insert_test3;
-INSERT INTO insert_test SELECT * FROM insert_test4;
-INSERT INTO insert_test SELECT * FROM insert_test5;
